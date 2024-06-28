81f3f590ea216e178ad1a85da3e9cd508970fb1e
Dave Pawlowski <dpawlows@emich.edu>
2024-06-28 12:29:05 -0400

    added mars timing stuff and a few more options to location plotter

--- srcPython/gitm_plot_one_loc.py
+++ srcPython/gitm_plot_one_loc.py
@@ -8,10 +8,12 @@ import matplotlib.pyplot as pp
 import matplotlib.dates as mdates
 from matplotlib.gridspec import GridSpec
 from gitm_routines import *
-import re
 import sys
+from marstiming import getMTfromTime
+
 
 rtod = 180.0/3.141592
+marsDay = 1.02749125 * 86400
 
 def get_args(argv):
 
@@ -22,9 +24,12 @@ def get_args(argv):
     alt = 400.0
     lon = -100.0
     lat = -100.0
+    alt = -100.0
     cut = 'loc'
     smin = -100.0
     smax = -100.0
+    lt = -100
+    average = -100.0
     help = 0
 
 
@@ -59,6 +64,21 @@ def get_args(argv):
             if m:
                 lon = int(m.group(1))
                 IsFound = 1
+    
+            m = re.match(r'-alt=(.*)',arg)
+            if m:
+                alt = int(m.group(1))
+                IsFound = 1
+
+            m = re.match(r'-lt=(.*)',arg)
+            if m:
+                lt = int(m.group(1))
+                IsFound = 1
+
+            m = re.match(r'-average=(.*)',arg)
+            if m:
+                average = int(m.group(1))
+                IsFound = 1
 
             m = re.match(r'-smin=(.*)',arg)
             if m:
@@ -90,16 +110,23 @@ def get_args(argv):
             'help':help,
             'lat':lat,
             'lon':lon,
+            'alt':alt,
             'IsLog':IsLog,
             'cut':cut,
             'smin':smin,
             'smax':smax,
-            'diff':diff}
+            'diff':diff,
+            'lt':lt,
+            'average':average}
 
     return args
 
 args = get_args(sys.argv)
 header = read_gitm_header(args["filelist"])
+averaging = False 
+if args['average'] > 0:
+    averaging = True
+    tAverage = args['average']
 
 if args['cut'] == 'loc' and args['lon'] > -50:
     plon = args['lon']
@@ -107,8 +134,20 @@ if args['cut'] == 'loc' and args['lon'] > -50:
 elif args['cut'] == 'sza' and args['smin'] > -50:
     smin = args['smin']
     smax = args['smax']
+elif args['cut'] == 'lt' and args['lat'] > -91:
+    plat = args['lat']
 else:
     args["help"] = '-h'    
+
+if args['cut'] == 'lt' and args['alt'] > 0:
+    lineplot = True
+    palt = args['alt']
+else:
+    lineplot = False 
+
+if averaging and args['lt'] < 0:
+    print('Time averaging can only be performed on lt plot type')
+    args["help"] = '-h' 
 
 if (args["help"]):
 
@@ -117,11 +156,14 @@ if (args["help"]):
     print('                     -help [file]')
     print('   -help : print this message')
     print('   -var=number[,num2,num3,...] : number is variable to plot')
-    print('   -cut=loc,sza: Plot type ')
+    print('   -cut=loc,sza,lt: Plot type ')
     print('   -lat=latitude : latitude in degrees (closest) (cut=loc) ')
     print('   -lon=longitude: longitude in degrees (closest) (cut=loc)')
+    print('   -alt=altitude: altitude in km (closest) (cut=lt) will create a line plot')
     print('   -smin=minsza: minimum solar zenith angle (cut=sza)')
     print('   -smax=maxsza: maximum solar zenigh angle (cut=sza)')
+    print('   -lt=localtime: nearest localtime to plot')
+    print('   -average=time: average a local time plot across time seconds')
     print('   -alog: plot the log of the variable')
     print('   -diff=backgroundFiles: plot the difference between 2 sets of files')
     print('   Non-KW args: files.')
@@ -144,10 +186,22 @@ try:
 except: 
     vars = [0,1,2]
 
+# Sort filenames based on year
+# Regular expression pattern to match 3D???_t', and a date and time stamp
+pattern = r'(3D..._t)(\d{6})_(\d{6})'
+
+# sorted_filenames = sorted(filelist, \
+#     key=lambda x: extract_year(x,pattern) if extract_year(x,pattern) is not None else float('inf'))
+
+fl = sorted(filelist, key=lambda x: extract_timestamp(x,pattern))
+filelist = fl 
+
 diff = False
 if args['diff'] != '0':
     diff = True
     backgroundFilelist = sorted(glob(args["diff"]))
+    fl = sorted(backgroundFilelist, key=lambda x: extract_timestamp(x,pattern))
+    backgroundFilelist = fl 
     nBackFiles = len(backgroundFilelist)
     if nBackFiles != nFiles:
         print('Difference between sizes of perturbation and background filelists:')
@@ -161,14 +215,19 @@ nvars = len(args['var'].split(','))
 
 #We want to store data for multiple variables, so we use a dict where var indices are the keys
 AllData = {a:[] for a in args['var'].split(',')}
+sum = []
 AllData2D = []
 AllAlts = []
 AllTimes = []
 AllSZA = []
 j = 0
+indexDayStart = []
+newday = True 
+
 for file in filelist:
 
     data = read_gitm_one_file(file, vars)
+    AllTimes.append(data["time"])
     
     if (j == 0):    
         [nLons, nLats, nAlts] = data[0].shape
@@ -178,8 +237,6 @@ for file in filelist:
 
         ialt1 = find_nearest_index(Alts,90)
         ialt2 = find_nearest_index(Alts,250)
-
-    AllTimes.append(data["time"])
 
     if diff:
         stime = str(AllTimes[-1].year)[2:]+str(AllTimes[-1].month).rjust(2,'0')+str(AllTimes[-1].day).rjust(2,'0')+\
@@ -219,11 +276,51 @@ for file in filelist:
 
             AllData[ivar].append(temp)
             # AllData[ivar].append(temp[mask].mean(axis=0))
-            
-    
-    
-    j+=1
+    if args['cut'] == 'lt':
+        marstime = getMTfromTime([data['time'].year,data['time'].month,data['time'].day,\
+            data['time'].hour,data['time'].minute,data['time'].second])
 
+        #subsolarlon is in degrees west so convert to east first
+        subsolarlon = 360 - marstime.subSolarLon
+        ltdiff = args['lt'] - 12  #subsolar is at 12:00 LT
+        plon = (subsolarlon + ltdiff*360/24) % 360
+        if lineplot:
+            ialt = find_nearest_index(Alts,palt)
+
+        ilon = find_nearest_index(Lons,plon)
+        ilat = find_nearest_index(Lats,plat)
+        tempData =  {a:[] for a in args['var'].split(',')}
+
+        for ivar in args['var'].split(','):
+            if lineplot:
+                AllData[ivar].append(data[int(ivar)][ilon,ilat,ialt])
+
+            else:
+
+                if diff:
+                    temp = (data[int(ivar)][ilon,ilat,ialt1:ialt2+1]-background[int(ivar)][ilon,ilat,ialt1:ialt2+1])/ \
+                        background[int(ivar)][ilon,ilat,ialt1:ialt2+1]*100.0
+                else:
+                    temp = data[int(ivar)][ilon,ilat,ialt1:ialt2+1]          
+        
+                if averaging:
+                    if newday:
+                        sum = temp
+                        newday = False
+                        indexDayStart.append(j)
+                        aveTStart = AllTimes[j]
+                    else:
+                        sum = sum + temp 
+                    if (data['time'] - aveTStart).total_seconds() > marsDay:
+                        newday = True
+                        AllData[ivar].append(sum/(j-indexDayStart[-1]+1))
+                        sum = []
+                        
+                else:
+                    AllData[ivar].append(temp)
+
+
+    j+=1
 
 
 for ivar in args['var'].split(','):
@@ -240,30 +337,45 @@ Alts = Alts[ialt1:ialt2+1]
 
 cmap = 'plasma'
 i=0
+
+if averaging:
+    averageDayStart = ((np.asarray(indexDayStart[0:-1])+np.asarray(indexDayStart[1:]))/2).astype(int)
+    Times = [AllTimes[i] for i in averageDayStart]
+else:
+    Times = AllTimes 
+
+fig, ax = plt.subplots(len(Var), 1, sharex=True)
 for ivar in args['var'].split(','):
-    ax = pp.subplot(6,1,i+1)
+
     AllData2D = AllData[ivar]
     if ivar == '3' and (not diff):
         AllData2D = np.log10(AllData2D)
         Var[i] = "Log "+ Var[i]
 
-    cont = ax.contourf(AllTimes,Alts,np.transpose(AllData2D),levels=30,cmap=cmap)    
-    if diff:
-        label = '{}\n% Diff'.format(Var[i])
+    if lineplot:
+        ax[i].plot(Times,AllData2D)
+        ax[i].set_ylabel(name_dict[Var[i]])
     else:
-        label = Var[i]
+        cont = ax[i].contourf(Times,Alts,np.transpose(AllData2D),levels=30,cmap=cmap)    
+        if diff:
+            label = '{}\n% Diff'.format(Var[i])
+        else:
+            label = Var[i]
 
-    pp.colorbar(cont,ax=ax,label=label)
-    if i < len(Var)-1:
-        ax.get_xaxis().set_ticklabels([])
-    
-    pp.ylabel('Alt (km)')
+        pp.colorbar(cont,ax=ax[i],label=label)
+       
+        
+        pp.ylabel('Alt (km)')
 
-    i+=1
+    # if i < len(Var)-1:
+    #     ax.get_xaxis().set_ticklabels([])
+    #     breakpoint()
+
+    i += 1
 
 pp.xlabel('Time (UT)')
-myFmt = mdates.DateFormatter("%b %d %H:%M")
-ax.xaxis.set_major_formatter(myFmt)
+myFmt = mdates.DateFormatter("%y-%m-%d")
+ax[-1].xaxis.set_major_formatter(myFmt)
 fig.autofmt_xdate()
 
 pp.savefig('plot.png')
