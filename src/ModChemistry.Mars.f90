7a1013c15c2ff0b88b23b6a52cf38fa435d5b890
Dave Pawlowski <dpawlows@emich.edu>
2025-02-21 11:18:58 -0500

    testing

--- src/ModChemistry.Mars.f90
+++ src/ModChemistry.Mars.f90
@@ -267,7 +267,7 @@ subroutine calc_chemical_sources(iLon,iLat,iAlt,iBlock,IonSources, &
               ! CO2 + hv ==> 2O + C
               ! ----------------------------------------------------------
               Reaction = EuvDissRateS(iLon,iLat,iAlt,iPDCO2_2O_C,iBlock)*Neutrals(iCO2_)
-!              Reaction = 0.0
+              Reaction = 0.0
               NeutralLosses(iCO2_) = NeutralLosses(iCO2_) + Reaction
               NeutralSources(iO_) = NeutralSources(iO_) + Reaction * 2
               NeutralSources(iC_) = NeutralSources(iC_) + Reaction
@@ -298,7 +298,7 @@ subroutine calc_chemical_sources(iLon,iLat,iAlt,iBlock,IonSources, &
               ! ----------------------------------------------------------
               Reaction = EuvDissRateS(iLon,iLat,iAlt,iPDCO_C_O,iBlock)*Neutrals(iCO_)
               
-!              Reaction = 0.0
+              Reaction = 0.0
               NeutralLosses(iCO_) = NeutralLosses(iCO_) + Reaction
               NeutralSources(iO_) = NeutralSources(iO_) + Reaction
               NeutralSources(iC_) = NeutralSources(iC_) + Reaction
@@ -596,7 +596,7 @@ subroutine calc_chemical_sources(iLon,iLat,iAlt,iBlock,IonSources, &
               ! O2 + C ==> CO + O(4S)
               ! ----------------------------------------------------------
               
-              reaction = rtO2_C * Neutrals(iO2_)*Neutrals(iC_)
+              reaction = rtO2_C * Neutrals(iO2_)*Neutrals(iC_)*10
 !              reaction = 0
               NeutralLosses(iO2_) = NeutralLosses(iO2_) + Reaction 
               NeutralLosses(iC_) = NeutralLosses(iC_) + Reaction 
