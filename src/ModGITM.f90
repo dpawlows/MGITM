c2f1b8c325d17685e99c49009a8da89e56fbc548
Dave Pawlowski <dpawlows@emich.edu>
2025-06-26 13:57:48 -0400

    updated chapman calculation to use E-GITM interpolation

--- src/ModGITM.f90
+++ src/ModGITM.f90
@@ -138,6 +138,7 @@ real, allocatable :: SpeciesDensityOld(:,:,:,:,:)
   real, allocatable :: PhotoEFluxD(:,:,:,:,:)
 
   real, allocatable :: PhotoEFluxTotal(:,:,:,:,:)
+  real, allocatable :: ColumnIntegralRho(:,:,:)
 
   real, dimension(nPhotoBins)                 :: PhotoEBins
   real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2) :: TempUnit
@@ -173,6 +174,7 @@ contains
     allocate(dAltDLon_CB(nLons,nLats,nAlts,nBlocks))
     allocate(dAltDLat_CB(nLons,nLats,nAlts,nBlocks))
     allocate(Rho(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(ColumnIntegralRho(-1:nLons+2, -1:nLats+2, -1:nAlts+2))
     allocate(Temperature(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
     allocate(VISC_3D(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
     allocate(InvScaleHeight(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
@@ -266,6 +268,7 @@ contains
     deallocate(dAltDLon_CB)
     deallocate(dAltDLat_CB)
     deallocate(Rho)
+    deallocate(ColumnIntegralRho)
     deallocate(Temperature)
     deallocate(VISC_3D)
     deallocate(InvScaleHeight)
